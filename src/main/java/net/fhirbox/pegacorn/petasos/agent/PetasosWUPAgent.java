af57bb8dca83cdf960a38a0ffe10e5a673eee473
Mark A. Hunter <m.a.hunter@outlook.com>
2020-04-27 20:09:25 +1000

    first commit to initialise repository

---
+++ src/main/java/net/fhirbox/pegacorn/petasos/agent/PetasosWUPAgent.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.fhirbox.pegacorn.petasos.agent;
+
+/**
+ *
+ * @author mhunter
+ */
+public class PetasosWUPAgent implements PetasosWUPInterface{
+    
+}
